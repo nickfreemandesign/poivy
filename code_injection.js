d0e982f80fff4f6b209f0188b5e2210626f8e0d5
Nick Freeman <nick@breadboard.com>
2022-06-05 13:52:01 -0400

    textile colors at-a-glance feature complete

---
+++ code_injection.js
@@ -0,0 +1,472 @@
+const BLUE_HEX = "#868eb0"
+const RED_HEX = "#be7672"
+const GREEN_HEX = "#a0ad96"
+const WHITE_HEX = "#e6dcd2"
+const YELLOW_HEX = "#fed071"
+const SALMON_HEX = "salmon"
+const TURQUOISE_HEX = "#7ce7e7"
+const LIME_HEX = "#d8ee9b"
+const CHILI_RED_HEX = "#ce4244"
+
+const BLANKETS = [
+    {
+        id: "thumb-mudcloth-and-wool-blanket-77ze8",
+        type: "Travel Blanket",
+        name: "Carapuegá",
+        colors: [
+            {
+                name: "salmon",
+                hex: SALMON_HEX,
+                width: "40px"
+            }, {
+                name: "turquoise",
+                hex: TURQUOISE_HEX,
+                width: "10px"
+            }, {
+                name: "salmon",
+                hex: SALMON_HEX,
+                width: "20px"
+            }, {
+                name: "turquoise",
+                hex: TURQUOISE_HEX,
+                width: "10px"
+            }, {
+                name: "salmon",
+                hex: SALMON_HEX,
+                width: "20px"
+            },
+        ]
+    },
+    {
+        id: "thumb-mali-carmen-african-mudcloth-yc4a9",
+        type: "Travel Blanket",
+        name: "Caacupé",
+        colors: [
+            {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "20px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "15px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "15px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "15px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "40px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "15px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "15px"
+            },
+        ]
+    },
+    {
+        id: "thumb-red-and-blue-cotton-travel-blanket",
+        type: "Travel Blanket",
+        name: "Asunción",
+        colors: [
+            {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "10px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "30px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "10px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "10px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "30px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "10px"
+            },
+        ]
+    },
+    {
+        id: "thumb-african-baule-mudcloth-h8mw9",
+        type: "Travel Blanket",
+        name: "Iguazú",
+        colors: [
+            {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "50px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "15px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "15px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "15px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "30px"
+            }
+        ]
+    },
+    {
+        id: "thumb-logwood-linen-pestemal-97cn3",
+        type: "Travel Blanket",
+        name: "Paraguarí",
+        colors: [
+            {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "30px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "10px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "20px"
+            },
+        ]
+    },
+    {
+        id: "thumb-swell-rope-bowl-m3lp3",
+        type: "Travel Blanket",
+        name: "Encarnación",
+        colors: [
+            {
+                name: "red",
+                hex: RED_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "30px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "30px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "20px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "10px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "20px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "10px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "10px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "yellow",
+                hex: YELLOW_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "10px"
+            },
+        ]
+    },
+    {
+        id: "thumb-dd72mnifukx6alnr822ftusykmc1r5",
+        type: "Travel Blanket",
+        name: "Caaguazú",
+        colors: [
+            {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "50px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "20px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "20px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "30px"
+            },
+        ]
+    },
+    {
+        id: "thumb-indigo-linen-pestemal-cskx9",
+        type: "Travel Blanket",
+        name: "Ybycuí",
+        colors: [
+            {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "20px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "15px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "15px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "20px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "20px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "20px"
+            },
+        ]
+    },
+]
+
+const BANDANAS = [
+    {
+        id: "thumb-tame-your-beast",
+        type: "Pet Bandana",
+        name: "Adventure Days",
+        colors: [
+            {
+                name: "lime",
+                hex: LIME_HEX,
+                width: "30px"
+            }, {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "15px"
+            }, {
+                name: "chili red",
+                hex: CHILI_RED_HEX,
+                width: "40px"
+            },
+        ]
+    },
+    {
+        id: "thumb-park-days",
+        type: "Pet Bandana",
+        name: "Park Days",
+        colors: [
+            {
+                name: "salmon",
+                hex: SALMON_HEX,
+                width: "20px"
+            }, {
+                name: "turquoise",
+                hex: TURQUOISE_HEX,
+                width: "20px"
+            }, {
+                name: "salmon",
+                hex: SALMON_HEX,
+                width: "50px"
+            },
+        ]
+    },
+    {
+        id: "thumb-lazy-days",
+        type: "Pet Bandana",
+        name: "Lazy Days",
+        colors: [
+            {
+                name: "blue",
+                hex: BLUE_HEX,
+                width: "10px"
+            }, {
+                name: "green",
+                hex: GREEN_HEX,
+                width: "20px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "20px"
+            }, {
+                name: "red",
+                hex: RED_HEX,
+                width: "20px"
+            }, {
+                name: "white",
+                hex: WHITE_HEX,
+                width: "20px"
+            }, 
+        ]
+    },
+]
+
+const PRODUCTS = BLANKETS.concat(BANDANAS)
+
+PRODUCTS.forEach(blanket => {
+    const thumbDiv = document.getElementById(blanket.id)
+    const [titleSection] = thumbDiv.getElementsByTagName("section")
+    titleSection.style.textAlign = "left"
+
+    const [titleContainer] = titleSection.getElementsByClassName("grid-main-meta")
+    const [title] = titleSection.getElementsByClassName("grid-title")
+
+    const colorSwatchContainer = document.createElement("div")
+    colorSwatchContainer.className = "color-swatch"
+    const { style: swatchStyle } = colorSwatchContainer
+    swatchStyle.display = "flex"
+    swatchStyle.flexDirection = "row"
+
+    const productType = document.createElement("span")
+    const { style: ptStyle } = productType
+    productType.innerText = blanket.type
+    ptStyle.fontSize = "calc((1.6 - 1) * 1.2vh + .3rem)"
+    ptStyle.marginLeft = "10px"
+    title.appendChild(productType)
+
+    const productDetail = document.createElement("p")
+    productDetail.style.margin = "2px 0"
+    const uniqueColors = [...new Set([...blanket.colors.map(({ name }) => name)])]
+    productDetail.innerText = uniqueColors.reduce((colors, color) => {
+        if (colors === "") {
+            colors = color
+        } else {
+            colors = `${colors} / ${color}`
+        }
+        return colors
+    }, "")
+    titleContainer.appendChild(productDetail)
+
+    blanket.colors.forEach((color, idx) => {
+        const colorBlock = document.createElement("div")
+        const { style: cbStyle } = colorBlock
+        cbStyle.width = color.width
+        cbStyle.height = "10px"
+        cbStyle.backgroundColor = color.hex
+        cbStyle.borderTop = "1px solid black"
+        cbStyle.borderBottom = "1px solid black"
+        if (idx === 0 ) {
+            cbStyle.borderLeft = "1px solid black"
+        } else if (idx === blanket.colors.length - 1 ){
+            cbStyle.borderRight = "1px solid black"
+        }
+        colorSwatchContainer.appendChild(colorBlock)
+    })
+
+    titleContainer.appendChild(colorSwatchContainer)
+
+})
+
